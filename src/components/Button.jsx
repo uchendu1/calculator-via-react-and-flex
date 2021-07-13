426a38cde2b66ee47cd4b2248aa3f367e9177950
linda <linda@lindas-MacBook-Air.local>
2021-07-13 09:50:00 +0700

    added mathjs dependency

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
 // checking if its an operator ie NaN
-const isOperator = val => {
+const isOperator = (val) => {
+//  return  (typeof val !== "number") || val === "." || val === "="
   return !isNaN(val) || val === "." || val === "=";
-}
 
-export const Button = props => <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>{props.children}</div>
+};
 
+
+
+export const Button = (props) => (
+  <div
+    className={`button-wrapper ${
+      isOperator(props.children) ? null : "operator"
+    }`}
+    onClick={() => props.handleClick(props.children)}
+  >
+    {props.children}
+  </div>
+);
